ae05cf5279c854c679861afd43a3a19b825868a3
Valeria Nemirova <val.pho.35@gmail.com>
2025-10-01 20:49:26 +0300

    last changes

--- src/components/login.js
+++ src/components/login.js
@@ -1,5 +1,31 @@
 export class Login {
     constructor() {
-        // console.log(Вход);
+        this.emailElement = document.getElementById('email');
+        this.passwordElement = document.getElementById('password');
+        document.getElementById('process-button').addEventListener('click', this.login.bind(this));
+    }
+
+    validateForm() {
+        let isValid = true;
+        if (this.emailElement.value && this.emailElement.value.match(/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/)) {
+            this.emailElement.classList.remove('is-invalid');
+        } else {
+            this.emailElement.classList.add('is-invalid');
+            isValid = false;
+        }
+
+        if (this.passwordElement.value) {
+            this.passwordElement.classList.remove('is-invalid');
+        } else {
+            this.passwordElement.classList.add('is-invalid');
+            isValid = false;
+        }
+
+        return isValid;
+
+    }
+
+    login () {
+        this.validateForm();
     }
 }
--- src/components/registration.js
+++ src/components/registration.js
@@ -1,5 +1,56 @@
 export class Registration {
     constructor() {
-        // console.log(Регистрация);
+        this.nameElement = document.getElementById('name');
+        this.lastNameElement = document.getElementById('last-name');
+        this.emailElement = document.getElementById('email');
+        this.passwordElement = document.getElementById('password');
+        this.confirmPasswordElement = document.getElementById('confirm-password');
+        document.getElementById('process-button').addEventListener('click', this.registration.bind(this));
+    }
+
+    validateForm() {
+        let isValid = true;
+
+        if (this.nameElement.value) {
+            this.nameElement.classList.remove('is-invalid');
+        } else {
+            this.nameElement.classList.add('is-invalid');
+            isValid = false;
+        }
+
+          if (this.lastNameElement.value) {
+            this.lastNameElement.classList.remove('is-invalid');
+        } else {
+            this.lastNameElement.classList.add('is-invalid');
+            isValid = false;
+        }
+
+        if (this.emailElement.value && this.emailElement.value.match(/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/)) {
+            this.emailElement.classList.remove('is-invalid');
+        } else {
+            this.emailElement.classList.add('is-invalid');
+            isValid = false;
+        }
+
+        if (this.passwordElement.value && this.passwordElement.value.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)) {
+            this.passwordElement.classList.remove('is-invalid');
+        } else {
+            this.passwordElement.classList.add('is-invalid');
+            isValid = false;
+        }
+
+        if (this.confirmPasswordElement.value && this.confirmPasswordElement.value === this.passwordElement.value) {
+            this.confirmPasswordElement.classList.remove('is-invalid');
+        } else {
+            this.confirmPasswordElement.classList.add('is-invalid');
+            isValid = false;
+        }
+
+        return isValid;
+
+    }
+
+    registration () {
+        this.validateForm();
     }
 }
